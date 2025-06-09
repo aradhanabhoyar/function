0d47c53e8097695f8fd2a0aba02db40a9a71d1f9
aradhanabhoyar <168920602+aradhanabhoyar@users.noreply.github.com>
2025-06-09 22:52:06 +0530

    Add files via upload

---
+++ method.py
@@ -0,0 +1,20 @@
+l = ["ravi",78,"purva",10.5,True]
+l.remove(78)            
+print(l)                        
+l.remove(l[3])                  
+print(l)
+l.remove(l[5])                 
+print(l)                        
+l.remove(l[1],"purva")        
+print(l)
+
+
+l.pop()
+print(l)
+l.pop(2)
+print(l)
+l.pop(4)               
+print(l)
+l.pop(l[2],"purva")        
+print(l)
+
